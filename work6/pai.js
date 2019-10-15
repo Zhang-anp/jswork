e216429efb769b09211a921e514d3a45e60267c4
Zhang-anp <1125015381@qq.com>
2019-10-15 15:43:13 +0800

    work6

---
+++ work6/pai.js
@@ -0,0 +1,30 @@
+function calculate(){
+    let num = document.getElementById('text').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('text').value = pi(num)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i= 1=i;1<=n;++i){
+        s
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for (let i=1;2n+1;++i){
+        s
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for (let i=0;i<=num;++i){
+        let dividend =factorial()
+        let dividend =oddFactorial()
+        s
+    }
+    return sum*2
+}
