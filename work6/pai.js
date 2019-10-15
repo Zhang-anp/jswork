7b7723e72ddf3a702beb66f6903bbd4932c67530
Zhang-anp <1125015381@qq.com>
2019-10-15 16:16:06 +0800

    work6

--- work6/pai.js
+++ work6/pai.js
@@ -1,30 +1,32 @@
 function calculate(){
-    let num = document.getElementById('text').value
+    let num = document.getElementById('num').value
     num = parseInt(num)&&Number(num)
     if(!isNaN(num)){
-        document.getElementById('text').value = pi(num)
+        document.getElementById('pai').value = pi(num)
     }
 }
 function factorial(n){
     let sum = 1
-    for(let i= 1=i;1<=n;++i){
-        s
+    for(let i=1;i<=n;++i){
+        sum *=i
     }
     return sum
 }
 function oddFactorial(n){
     let sum = 1
-    for (let i=1;2n+1;++i){
-        s
+    for (let i=1;i<=n;++i){
+        sum *= 2*i+1
+
     }
     return sum
 }
 function pi(num){
     let sum = 0
     for (let i=0;i<=num;++i){
-        let dividend =factorial()
-        let dividend =oddFactorial()
-        s
+        let dividend =factorial(i)
+        let divisor =oddFactorial(i)
+        sum +=dividend/divisor
     }
-    return sum*2
+    return sum *2
 }
+calculate()
