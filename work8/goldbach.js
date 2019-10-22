1285dd6c72f8a8c491577e0a2b67be80b5d1dd35
Zhang-anp <1125015381@qq.com>
2019-10-22 15:46:23 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,20 +1,24 @@
-
-function goldbach(num){
-    var sum =0
-    for (var d=1;d<=num;d++){
-         if(num % d==0){
-             sum ++
-         }
-    }
-    if(sum==2){
-        return true
-    }else {
-        return false
-    }
-}
-for (var b=2;b<add;b++){
-    var c=a-b
-    if(goldbach(b) && goldbach(c) && b<=c){
-        console.log(a + "可以拆分为两个质数" + b + "+" c)
-    }
-}
+function goldbach() {     var value = document.getElementById('odd').value 
+    if(value%2!==0){         alert("请输入偶数")     }   
+      let goldbach = document.getElementById('goldbach')   
+        var arr =[]   
+          var a = 0;    
+         for(var i=2;i<=value;i++){  a = 0   
+             for(var j=2;j<i;j++){ if(i%j==0){
+                  a++
+            }        
+        }     
+            if(a==0){arr.push(i) 
+                  
+            }   
+                  
+        }     
+        var str = ''  
+        for(let i=0;i<(arr.length)/2;i++){      
+        for(let j=0;j<arr.length;j++){          
+        if((arr[i]+arr[j])===Number(value)){   
+           str += '<div>'+value+"可以拆分为两个质数"+arr[i]+"与"+arr[j]+"的和"+ '</div>'    
+       }    
+   }   
+ }    
+  goldbach.innerHTML = str }
