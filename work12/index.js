223be619a6e626dc9572af585014b578d205982c
Zhang-anp <1125015381@qq.com>
2019-11-25 22:09:22 +0800

    work12

--- work12/index.js
+++ work12/index.js
@@ -1,16 +1,6 @@
-let year = new Data().getFullYear
+let year = new Date().getFullYear();
+document.getElementById("cal").innerHTML = calendar(year);
 
-document.getElementById('cal').innerHTML = '<div class="box" id="cal"></div>'
-
-function setCalender(year){
-    var year1 = new Date().getFullYear()
+function setCalendar(year) {
     
-    var pre = year1-1
-    var next = year1+1
-    document.getElementById('curyear').setAttribute('data-year', year1);
-    document.getElementById('preyear').setAttribute('data-year', pre);
-    document.getElementById('nextyear').setAttribute('data-year', next);
-    document.getElementById('curyear').innerText = year1
-    document.getElementById('preyear').innerText = "<<"+pre
-    document.getElementById('nextyear').innerText = next+ ">>"
 }
