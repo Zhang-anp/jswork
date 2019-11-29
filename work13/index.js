e286ad558604323fcd5ccb9fa4feb406bbddb114
Zhang-anp <1125015381@qq.com>
2019-11-29 10:44:45 +0800

    work13

--- work13/index.js
+++ work13/index.js
@@ -7,7 +7,7 @@ var elements = [
     {tag:'input',text:'爱    好',attr:{type:'checkbox',name:'hobby[]'},
     option:{swimming:'游泳',reading:'读书',running:'跑步'}},
     {tag:'select',text:'住    址',attr:{name:'area'},
-    option:{'':'--请选择--',bj:'北京',sh:'上海',sz:'深圳'}},
+    option:{'':'--请选择--',bj:'北京',sh:'上海',sz:'深圳',gz:'广州'}},
     {tag:'textarea',text:'自我介绍:',
         attr: {name:'introduce',cols:'50',row:'5'}},
     {tag:'input',attr:{type:'submit',value:'提交'}}
