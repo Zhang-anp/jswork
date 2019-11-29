01fb198e2e19841bbaaf1a1287da63aa2380b9ed
Zhang-anp <1125015381@qq.com>
2019-11-29 13:15:05 +0800

    work13

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser")
 
-app.use(express.static(','))
+app.use(express.static('.'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false}));
 app.post('/formBuilder', function (req,res){
